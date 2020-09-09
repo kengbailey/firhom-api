469637ac4a5b2456957d5c4924571c23407fd9ca
Ken Bailey <kgbailey92@gmail.com>
2020-09-09 15:46:39 -0500

    working on database functions

--- main.go
+++ main.go
@@ -29,31 +29,35 @@ func processWavHandler(w http.ResponseWriter, r *http.Request) {
 	cmdOut, err := cmd.Output()
 	if err != nil {
 		log.Printf("Failed to execute youtube-dl command: %v\n", err)
-		// TODO: return error
+		http.Error(w, "Failed to download WAV!", http.StatusBadRequest)
+		return
 	}
 	vidInfo := strings.Split(string(cmdOut), "|")
 
 	// create db record
-	statement, err := db.Prepare("INSERT INTO waves (youtube_url, youtube_id, wave_name) VALUES (?, ?, ?)")
+	// TODO: Check for existing record before we insert new record
+	statement, err := db.Prepare("INSERT INTO waves (youtube_url, youtube_id, wave_name) VALUES (?, ?, ?);")
 	if err != nil {
 		log.Printf("Failed to insert new record into sqlite3 database: %v\n", err)
-		// TODO: return error
+		http.Error(w, "Failed to download WAV!", http.StatusBadRequest)
+		return
 	}
 	statement.Exec(videoURL, vidInfo[0], vidInfo[1])
 
-	// kick off ytdl to wav --> youtube-dl -f bestaudio --extract-audio --audio-format wav --audio-quality 0 --output 123.wav <Video-URL>
+	// kick off ytdl to wav --> youtube-dl -f bestaudio --extract-audio --audio-format wav --audio-quality 0 --output "./wavs/%(id)s.%(ext)s" <Video-URL>
 	// We don't wait for this to finish
 	cmd = exec.Command(`youtube-dl`,
 		`-f`, `bestaudio`,
 		`--extract-audio`,
 		`--audio-format`, `wav`,
 		`--audio-quality`, `0`,
-		`--output`, `%(id)s.%(ext)s`,
+		`--output`, `./wavs/%(id)s.%(ext)s`,
 		videoURL)
 	err = cmd.Start()
 	if err != nil {
 		log.Printf("Failed to execute youtube-dl command: %v\n", err)
-		// TODO: return error
+		http.Error(w, "Failed to download WAV!", http.StatusBadRequest)
+		return
 	}
 
 	return
@@ -66,22 +70,39 @@ func downloadWavHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// get wav id
-	file := r.URL.Query()["id"][0]
+	wavID := r.URL.Query()["id"][0]
 
 	// get record from db
+	var fileID string
+	err := db.QueryRow("select youtube_id from waves where id=?", wavID).Scan(&fileID)
+	if err != nil {
+		log.Printf("Failed to retrieve wav from sqlite database: %v", err)
+		http.Error(w, "Failed to find WAV!", http.StatusNotFound)
+		return
+	}
 
 	// download file to user
 	w.Header().Set("Content-Type", "application/octet-stream")
-	w.Header().Set("Content-Disposition", "attachment; filename="+file)
+	w.Header().Set("Content-Disposition", "attachment; filename="+fileID+".wav")
 	// todo: content length
-	diskFile, err := os.Open(fmt.Sprintf("./dl/%s", file))
+	diskFile, err := os.Open(fmt.Sprintf("./wavs/%s.wav", fileID))
 	if err != nil {
+		log.Printf("Failed to open wav file on disk: %v", err)
 		http.Error(w, "Failed to open file!", http.StatusNotFound)
+		return
 	}
 	defer diskFile.Close()
 	io.Copy(w, diskFile)
 
 	return
+}
+
+// wavListItem ... returned to UI upon page load.
+type wavListItem struct {
+	ID     int
+	Name   string
+	URL    string
+	DlDate string
 }
 
 func getWavListHandler(w http.ResponseWriter, r *http.Request) {
@@ -91,8 +112,20 @@ func getWavListHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// fetch from db
+	var WavList []wavListItem
+	rows, err := db.Query("Select id, wav_name, youtube_url, insert_dt from waves;")
+	if err != nil {
+		log.Println("Failed to retrieve list of waves: %v", err)
+		http.Error(w, "Failed to retrieve wav list!", http.StatusNotFound)
+		return
+	}
+	for rows.Next() {
+		var tempItem wavListItem
+		rows.Scan(&tempItem.ID, &tempItem.Name, &tempItem.URL, &tempItem.DlDate)
+		WavList = append(WavList, tempItem)
+	}
 
-	// return list
+	// return json marshalled list
 
 	return
 }
@@ -111,6 +144,7 @@ func initDb() {
 		log.Fatal("Failed to initialize waves table: %v", err)
 	}
 	statement.Exec()
+	statement.Close()
 }
 
 func main() {
--- test.go
+++ test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -16,13 +18,31 @@ func main() {
 	// vidInfo := strings.Split(string(cmdOut), "|")
 	// fmt.Println(vidInfo[0], vidInfo[1])
 
-	database, _ := sql.Open("sqlite3", "./waverunner.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS waves (id INTEGER PRIMARY KEY, youtube_url NOT NULL TEXT, youtube_id NOT NULL TEXT, wave_name NOT NULL TEXT, insert_dt TEXT DEFAULT CURRENT_TIMESTAMP)")
-	statement.Exec()
+	database, err := sql.Open("sqlite3", "./waverunner.db")
+	if err != nil {
+		log.Fatalf("Failed to open sqlite database connection: %v\n", err)
+	}
+	// statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS waves (id INTEGER PRIMARY KEY, youtube_url TEXT NOT NULL, youtube_id TEXT NOT NULL, wave_name TEXT NOT NULL, insert_dt DATETIME DEFAULT CURRENT_TIMESTAMP);")
+	// if err != nil {
+	// 	log.Fatalf("Failed to execute sqlite statement: %v", err)
+	// }
+	// statement.Exec()
 
-	statement, _ = database.Prepare("INSERT INTO waves (youtube_url, youtube_id, wave_name) VALUES (?, ?, ?)")
-	statement.Exec("Nic", "Raboy")
+	// statement, err := database.Prepare("INSERT INTO waves (youtube_url, youtube_id, wave_name) VALUES (?, ?, ?)")
+	// if err != nil {
+	// 	log.Fatalf("Failed to insert new item into the database: %v", err)
+	// }
+	// statement.Exec("one", "two", "three")
 
+	rows, err := database.Query(fmt.Sprintf("Select youtube_id from waves where id = %d;", 1))
+	if err != nil {
+		log.Fatalf("Failed to select item from database: %v", err)
+	}
+	var youtubeURL string
+	for rows.Next() {
+		rows.Scan(&youtubeURL)
+		fmt.Println(youtubeURL)
+	}
 	// rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
 	// var id int
 	// var firstname string
@@ -39,9 +59,9 @@ What do we need for this db?
 Table Name --> Waves
 
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-youtube_url NOT NULL TEXT,
-youtube_id NOT NULL TEXT,
-wave_name NOT NULL TEXT,
-insert_dt TEXT DEFAULT CURRENT_TIMESTAMP
+youtube_url TEXT NOT NULL,
+youtube_id TEXT NOT NULL,
+wave_name TEXT NOT NULL,
+insert_dt DATETIME DEFAULT CURRENT_TIMESTAMP
 
 */
